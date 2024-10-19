f310c167555c748475901f9c70e6c8108e7b2c2d
Usman Zahid <developerusman@yahoo.com>
2024-10-19 23:20:29 +0500

    Initial commit!

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Usman Zahid";
+        System.out.println("Hello World! my name is " + name);
+
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println("i = " + i);
+//        }
+    }
+}
